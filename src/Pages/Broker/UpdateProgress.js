009afca6243fd6cccbdc365ef8491832564fff1b
Razin <razinnazarudin@gmail.com>
2024-04-17 00:45:07 +0800

    betulkan update progress

--- src/Pages/Broker/UpdateProgress.js
+++ src/Pages/Broker/UpdateProgress.js
@@ -4,10 +4,6 @@ export const UpdateProgress = () => {
   return (
     <div className="UpdateProgress">
       <h1>Update Progress</h1>
-      <p>
-        Kalau yang reject tu kau dekat css kau buat "display: none" dia tersorok
-        so guna lah js untuk tekan button hide kan and kalau tekan baru muncul
-      </p>
     </div>
   );
 };
