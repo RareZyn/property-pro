fa6fa0216d01c6459c73e80074ecacbc7e20666e
azim <u2102134@siswa.um.edu.my>
2024-04-17 17:46:25 +0800

    test push

---
+++ src/Pages/Property/Browser component/PropertyDisplayCard.js
@@ -0,0 +1,15 @@
+import "../Browser component/PropertyDisplayCard.css";
+
+export const PropertyDisplayCard = () => {
+    <div id="prop-card">
+        <h1>Test</h1>
+        <div id="img-placeholder">
+            
+        </div>
+        <div>
+            <h6>Item name</h6>
+            <p>RM XX.XX</p>
+        </div>
+    </div>
+}
+
--- src/Pages/Property/BrowserProperty.js
+++ src/Pages/Property/BrowserProperty.js
@@ -1,9 +1,11 @@
 import "./BrowserProperty.css";
+import {PropertyDisplayCard} from "./Browser component/PropertyDisplayCard";
 
 export const BrowserProperty = () => {
   return (
     <div className="BrowserProperty">
       <h1>Browser Property</h1>
+      <PropertyDisplayCard />
     </div>
   );
 };
