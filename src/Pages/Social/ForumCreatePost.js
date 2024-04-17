8811def57dc7d1b593ba2d30097b851e3a537d40
Razin <razinnazarudin@gmail.com>
2024-04-17 18:49:57 +0800

    Create post

--- src/Pages/Social/ForumCreatePost.js
+++ src/Pages/Social/ForumCreatePost.js
@@ -4,6 +4,20 @@ export const ForumCreatePost = () => {
   return (
     <div className="ForumCreatePost">
       <h1>Forum Create Post</h1>
+      <div className="topic-discuss">
+        <div className="user-profile">
+          <img
+            id="user-image"
+            src={require("../../Res/image/user profile.png")}
+          />
+          <section id="user-text">
+            <p id="user-name">Wan Haritah</p>
+            <p id="user-time">10 min ago</p>
+          </section>
+        </div>
+        <div className="user-text"></div>
+        <div className="user-button"></div>
+      </div>
     </div>
   );
 };
