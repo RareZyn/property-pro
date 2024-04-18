4c21d2b65f4b61d42e581c76683d64f17a997591
azim <u2102134@siswa.um.edu.my>
2024-04-19 01:11:39 +0800

    Browse property 90% done

--- src/Pages/Property/Browser component/PropertyDisplayCard.js
+++ src/Pages/Property/Browser component/PropertyDisplayCard.js
@@ -1,11 +1,12 @@
 import "../Browser component/PropertyDisplayCard.css";
+import imgPh from "../../../Res/image/IMG PH.png"
 
 export const PropertyDisplayCard = () => {
     return(
         <div id="prop-card" className="box-shadow">
             <div id="details">
                 <div id="img-placeholder">
-                   <img src="src/Res/image/IMG PH.png" alt="Image" srcset="" />
+                   <img src={imgPh} alt="Image" srcset="" id="img-ph"/>
                 </div>
                 <h4 className="txt40">Item name</h4>
                 <p className="txt24">RM XX.XX</p>
--- src/Pages/Property/BrowserProperty.js
+++ src/Pages/Property/BrowserProperty.js
@@ -1,9 +1,15 @@
 import "./BrowserProperty.css";
 import {PropertyDisplayCard} from "./Browser component/PropertyDisplayCard";
+import SearchBar from "../../Components/SearchBar";
+import backIcon from "../../Res/image/back-icon.png"
 
 export const BrowserProperty = () => {
   return (
     <div className="BrowserProperty">
+      <header>
+        <a href=""><img id="back-button" src={backIcon} alt=""/></a>
+        <SearchBar hint="Browse Property..."/>
+      </header>
       <div className="Properties-grid">
         <h1 id="title">Browser Property</h1>
         <PropertyDisplayCard />
