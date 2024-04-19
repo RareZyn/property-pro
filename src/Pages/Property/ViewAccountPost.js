465f6591b7604d45eafa7a3abacc45205c7e271e
azim <u2102134@siswa.um.edu.my>
2024-04-20 02:46:38 +0800

    Buat post card 90%, pastu tukar ChatPopup.css --> Chatpopup.modul.css

--- src/Pages/Property/ViewAccountPost.js
+++ src/Pages/Property/ViewAccountPost.js
@@ -1,9 +1,12 @@
 import "./ViewAccountPost.css";
+import PostCard from "../../Components/PostCard"
 
 export const ViewAccountPost = () => {
   return (
     <div className="ViewAccountPost">
       <h1>View Account Post</h1>
+      <PostCard />
+      <PostCard />
     </div>
   );
 };
