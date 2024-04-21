ecb4ffc21556f8398fba5ae338a5b7a48ccfdcfa
aisyah551 <nooraisyah551@gmail.com>
2024-04-22 04:22:25 +0800

    html, css

--- src/Pages/Property/PropertyDetails.js
+++ src/Pages/Property/PropertyDetails.js
@@ -3,7 +3,84 @@ import "./PropertyDetails.css";
 export const PropertyDetails = () => {
   return (
     <div className="PropertyDetails">
-      <h1>Property Details</h1>
+      <span class="back-icon">
+      </span>
+
+      <div class="main-img-div">
+          <div class="main-img">
+              <img/>
+          </div>
+      </div>
+
+      <div class="property-details">
+          <div class="other-imgs">
+              <div class="other-imgs-box">
+                  <img/>
+              </div>
+          </div>
+
+          <div class="item-div">
+              <div class="first-line-div">
+                  <span class="item-title">Item Title</span>
+                  <div class="share-icon">
+                    <div>
+                        <img src={require("../../Res/image/share-icon-black.png")}/>
+                    </div>
+                    <span class="shares-text">Shares</span>
+                  </div>
+              </div>
+              
+              <div class="seller-name">Seller</div>
+  
+              <div class="item-desc">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui incidunt quaerat totam eos alias rerum odio enim deleniti porro, culpa explicabo reprehenderit exercitationem aliquam molestias doloremque, error hic dolorum facilis.
+              </div>
+              
+              <div class="price-saved-div">
+                  <div class="invisible-element"></div>
+                  <button class="price-btn">Price RMXX.XX</button>
+                  <div class="bookmark-icon">
+                    <img src={require("../../Res/image/bookmark-icon.png")}/>
+                  </div>                
+              </div>
+          </div>
+
+          <div class="comments-section">
+              <div class="comments">Comments</div>
+
+              <div class="comments-container">
+                  <div class="commenter-details">
+                    <div class="icon-name-div">
+                            <img src={require("../../Res/image/user profile.png")}/>                           
+                        </div>
+                      <div>
+                          <div class="name">Wany Haritah</div>
+                          <div class="time-public-div">10 mins ago . Public</div>
+                      </div>
+                  </div>
+
+                  <div class="comment-text">
+                      Comment bla bla bla bla bla
+                  </div>
+              </div>
+
+              <div class="comments-container">
+                  <div class="commenter-details">
+                    <div class="icon-name-div">
+                          <img src={require("../../Res/image/user profile.png")}/>                           
+                        </div>
+                      <div>
+                          <div class="name">Wany Haritah</div>
+                          <div class="time-public-div">10 mins ago . Public</div>
+                      </div>
+                  </div>
+
+                  <div class="comment-text">
+                      Comment bla bla bla bla bla
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
   );
 };
--- src/Pages/general/HomePage.js
+++ src/Pages/general/HomePage.js
@@ -3,7 +3,75 @@ import "./HomePage.css";
 export const HomePage = () => {
   return (
     <div className="HomePage">
-      <h1>HomePage</h1>
+
+      <div class="homepage-img-grid">
+          <div class="homepage-img homepage-img-women">
+              <img src={require("../../Res/image/men-image1.png")}/>
+          </div>
+          <div class="homepage-img homepage-img-men" >
+              <img src={require("../../Res/image/women-image.png")}/>
+          </div>
+      </div>
+
+      <div class="buyer-seller-section">
+          <div class="buyer-seller-grid">
+              <a href="" class="text buyer">
+                  BUYER
+              </a>
+              <a href="" class="text seller">
+                  SELLER
+              </a>
+          </div>
+      </div>
+
+      <div class="community-grid">
+          <div class="img-div1">
+              <img src={require("../../Res/image/community-image.png")} class="community-img"/>
+          </div>
+          <div class="community-details">
+              <div class="community-heading">COMMUNITY</div>
+              <div class="community-desc">Join the discussion with multiple people around the globe.</div>
+              <a href="">
+                  <button class="homepage-btn">COMMUNITY</button>
+              </a>
+          </div>
+      </div>
+
+      <div class="broker-grid">
+        <div class="img-div2">
+            <img src={require("../../Res/image/broker-image.png")} class="broker-img"/>
+        </div>
+        <div class="broker-details">
+            <div class="broker-heading">Become our broker</div>
+            <div class="broker-desc">Are you a real estate agent in Malaysia?
+                Join us as our broker to help people in property transaction.
+            </div>
+            <div class="broker-btns">
+                <a href="">
+                    <button class="homepage-btn log-in-btn">LOG IN</button>
+                </a>
+                <a href="">
+                    <button class="homepage-btn">SIGN UP</button>
+                </a>
+            </div>
+        </div>
+      </div>
+
+      <div class="ad-grid">
+          <div class="img-div3">
+              <img src={require("../../Res/image/ad-image.png")} class="ad-img"/>
+          </div>
+          <div class="ad-details">
+              <div class="ad-heading">ADVERTISEMENT</div>
+              <div class="ad-desc">
+                  This part can be used as advertisement or any
+                  current issues or news to be updated for user to read
+                  or be notified.
+              </div>
+          </div>
+      </div>
+
+      <div class="extra-div"></div>
     </div>
   );
 };
