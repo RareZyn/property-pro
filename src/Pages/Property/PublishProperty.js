3ed62af50c0d90dda33bd562bc0ea1ae8fc405d3
Razin <razinnazarudin@gmail.com>
2024-04-23 00:43:04 +0800

    Betulkan sell

--- src/Pages/Property/PublishProperty.js
+++ src/Pages/Property/PublishProperty.js
@@ -1,6 +1,7 @@
 import "./PublishProperty.css";
 import { MyAccountHeader } from "../general/MyAccountHeader";
 import { Link } from "react-router-dom";
+
 export const PublishProperty = () => {
   return (
     <div className="PublishProperty">
@@ -9,48 +10,58 @@ export const PublishProperty = () => {
         bio="please guide me"
         currentPage="Property"
       />
-
-      <div class="for-top-margin"></div>
-
-      <Link to="/manage-property">
-        <span class="back-icon">
-          <img src={require("../../Res/image/back-icon.png")} />
-        </span>
-      </Link>
-
-      <div class="publish-section">
-        <div class="cart-box">
+      <div className="publish-section">
+        <div className="cart-box">
           <img src={require("../../Res/image/cart-icon.png")} />
         </div>
 
-        <div class="input-div">
-          <div>Name</div>
-          <input type="text" placeholder="Name" class="name-input" />
-          <div>Description</div>
-          <input type="text" placeholder="Description" class="desc-input" />
-          <div>Price</div>
-          <input type="text" placeholder="RMXX.XX" class="price-input" />
+        <div className="input-div">
+          <div className="section-input">
+            Name
+            <input type="text" placeholder="Name" className="name-input" />
+          </div>
+          <div className="section-input">
+            Description
+            <input
+              type="text"
+              placeholder="Description"
+              className="desc-input"
+            />
+          </div>
+          <div className="section-input">
+            Price
+            <input type="text" placeholder="RMXX.XX" className="price-input" />
+          </div>
         </div>
 
-        <div class="add-img-files-div">
-          <button class="add-img-files-btn">+</button>
-          <div class="add-img-files-text">Add images / files</div>
+        <div className="add-img-files-div">
+          <input
+            type="file"
+            id="myFile"
+            name="filename"
+            className="input-file"
+          />
+          <label for="myFile" className="custom-file-upload" id="addFile">
+            <img src={require("../../Res/image/upload.png")} alt="Upload" />
+            <h2>Add images from files</h2>
+            <h4>or drag and drop</h4>
+          </label>
         </div>
 
-        <div class="files-grid">
-          <div class="zip-box">
-            <div class="files-icon">
+        <div className="files-grid">
+          <div className="zip-box">
+            <div className="files-icon">
               <img src={require("../../Res/image/zip-file-icon.png")} />
             </div>
             <button class="view-btn">View</button>
 
-            <div class="file-edit-delete-div">
-              <span class="filename-zip">filename.zip</span>
-              <div class="delete-edit-icon-div">
-                <div class="delete-pic">
+            <div classNames="file-edit-delete-div">
+              <span className="filename-zip">filename.zip</span>
+              <div className="delete-edit-icon-div">
+                <div className="delete-pic">
                   <img src={require("../../Res/image/delete-icon.png")} />
                 </div>
-                <div class="edit-pic">
+                <div className="edit-pic">
                   <img src={require("../../Res/image/edit-icon.png")} />
                 </div>
               </div>
