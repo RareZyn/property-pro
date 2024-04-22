66d29e3f709cf8b6caa750ee0e74c4b729da9377
Razin <razinnazarudin@gmail.com>
2024-04-22 22:01:58 +0800

    Cantikkan homepage

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/navHeader" element={<NavHeader />} />
           <Route path="/HomePage" element={<HomePage />} />
--- src/Pages/Navigation/NavHeader.js
+++ src/Pages/Navigation/NavHeader.js
@@ -1,35 +1,34 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { DropdownMenuIcon } from "./DropdownMenuIcon.js";
-import { DropdownMenu } from './DropdownMenu.js';
+import { DropdownMenu } from "./DropdownMenu.js";
 import "./NavHeader.css";
-import { ChatPopup } from '../Social/ChatPopup.js';
+import { ChatPopup } from "../Social/ChatPopup.js";
 
 export const NavHeader = () => {
-
   const [burger_icon, setBurgerIcon] = useState("DropdownBar unclicked");
-  const [dropdownMenu, setDropdownMenu] = useState("menu hidden")
+  const [dropdownMenu, setDropdownMenu] = useState("menu hidden");
   const [isMenuClicked, setIsMenuClicked] = useState(true);
 
   const [isChatClicked, setIsChatClicked] = useState(true);
-  const [chatPopdown, setChatPopdown] = useState("ChatPopup hidden"); 
+  const [chatPopdown, setChatPopdown] = useState("ChatPopup hidden");
   const [chatNavigation, setChatNavigation] = useState("Navigator");
 
-  const showMenu = () =>{
-    if(isMenuClicked){
+  const showMenu = () => {
+    if (isMenuClicked) {
       setBurgerIcon("DropdownBar clicked");
       setDropdownMenu("menu visible");
-    } else{
+    } else {
       setBurgerIcon("DropdownBar uncliked");
-      setDropdownMenu('menu hidden');
+      setDropdownMenu("menu hidden");
     }
-    setIsMenuClicked(!isMenuClicked)
+    setIsMenuClicked(!isMenuClicked);
   };
 
-  const showChat = () =>{
-    if(isChatClicked){
+  const showChat = () => {
+    if (isChatClicked) {
       setChatPopdown("ChatPopup2");
       setChatNavigation("active");
-    } else{
+    } else {
       setChatPopdown("ChatPopup2 hidden");
       setChatNavigation("Navigator");
     }
@@ -38,32 +37,41 @@ export const NavHeader = () => {
 
   return (
     <div className="NavHeader">
-      <DropdownMenuIcon onClick={showMenu} className={burger_icon}></DropdownMenuIcon>
-      <a href='/' className="NavTitle">PropertyPro</a>
+      <DropdownMenuIcon
+        onClick={showMenu}
+        className={burger_icon}
+      ></DropdownMenuIcon>
+      <a href="/" className="NavTitle">
+        PropertyPro
+      </a>
       <DropdownMenu className={dropdownMenu}></DropdownMenu>
-      <nav className='NavigatorRightHeader'>
-      <ul>
-      <CustomLink href="/">Home</CustomLink>
-        <CustomLink href="/browser-property">Browser</CustomLink>
-        <CustomLink href="/publish-property">Sell</CustomLink>
-        <li className={chatNavigation} onClick={showChat}>Chat</li>
-        <CustomLink href="/forum-page">Community</CustomLink>
-        <CustomLink href="/register-broker">Broker</CustomLink>
+      <nav className="NavigatorRightHeader">
+        <ul>
+          <CustomLink href="/HomePage">Home</CustomLink>
+          <CustomLink href="/browser-property">Browser</CustomLink>
+          <CustomLink href="/publish-property">Sell</CustomLink>
+          <li className={chatNavigation} onClick={showChat}>
+            Chat
+          </li>
+          <CustomLink href="/forum-page">Community</CustomLink>
+          <CustomLink href="/register-broker">Broker</CustomLink>
 
-        <CustomLink href="/launch">Logout</CustomLink>
-      </ul>
+          <CustomLink href="/launch">Logout</CustomLink>
+        </ul>
       </nav>
       <ChatPopup className={chatPopdown}></ChatPopup>
     </div>
   );
 };
 
-function CustomLink({href,children,...props}){
+function CustomLink({ href, children, ...props }) {
   const path = window.location.pathname;
 
-  return(
-    <li className={path===href? "active" : ""}>
-      <a className='Navigator' href={href} {...props}>{children}</a>
+  return (
+    <li className={path === href ? "active" : ""}>
+      <a className="Navigator" href={href} {...props}>
+        {children}
+      </a>
     </li>
   );
 }
