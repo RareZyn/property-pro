f42e844980a20d89f5ed358f6a1bc54e6fbf6c81
azim <u2102134@siswa.um.edu.my>
2024-04-22 23:43:45 +0800

    update

--- src/Components/TruncatedText.jsx
+++ src/Components/TruncatedText.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./TruncatedText.css";
+import styles from "./TruncatedText.module.css";
 
 function TruncatedText({text, maxlength}){
     const [expanded, setExpanded] = useState(text.length <= maxlength);
@@ -14,7 +14,7 @@ function TruncatedText({text, maxlength}){
         (<span>{text}</span>):
         (<div>
             {text.slice(0, maxlength)}...
-            <span id="see-more" onClick={toggleExpanded}> See more</span>
+            <span id={styles["see-more"]} onClick={toggleExpanded}> See more</span>
         </div>)
         }
         </>
