64ed29bc02843ad4cea7314cf2b74e68a58d7e33
Razin <razinnazarudin@gmail.com>
2024-04-23 01:33:33 +0800

    Fix my account header

--- src/Pages/general/MyAccountHeader.js
+++ src/Pages/general/MyAccountHeader.js
@@ -23,48 +23,61 @@ export const MyAccountHeader = () => {
           <h1 style={{ fontSize: "1.7em" }}>{userDetails.username}</h1>
           <p style={{ fontSize: "1.0em" }}>{userDetails.bio}</p>
         </div>
-        <a href="manage-account">
-          <img src={require("../../Res/image/editicon.jpg")} alt="Edit icon" />
-        </a>
+        <div className="editButton">
+          <a href="manage-account">
+            <img
+              src={require("../../Res/image/editicon.jpg")}
+              alt="Edit icon"
+            />
+          </a>
+        </div>
       </section>
 
       <section id="account-link-container">
-        <a
-          className={`account-link ${
-            selectedLink === "property" ? "selected-acc-link" : ""
-          }`}
-          onClick={() => handleLinkClick("property")}
-          href="myaccount-property"
-        >
-          Property
-        </a>
-        <a
-          className={`account-link ${
-            selectedLink === "post" ? "selected-acc-link" : ""
-          }`}
-          onClick={() => handleLinkClick("post")}
-          href="myaccount-post"
-        >
-          Post
-        </a>
-        <a
-          className={`account-link ${
-            selectedLink === "transaction" ? "selected-acc-link" : ""
-          }`}
-          onClick={() => handleLinkClick("transaction")}
-          href="myaccount-transaction"
-        >
-          My Transaction
-        </a>
-        <a
-          className={`account-link ${
-            selectedLink === "details" ? "selected-acc-link" : ""
-          }`}
-          onClick={() => handleLinkClick("details")}
-          href="myaccount-details"
-        >
-          About
-        </a>
+        <div id="section">
+          <a
+            className={`account-link ${
+              selectedLink === "property" ? "selected-acc-link" : ""
+            }`}
+            onClick={() => handleLinkClick("property")}
+            href="myaccount-property"
+          >
+            Property
+          </a>
+        </div>
+        <div id="section">
+          <a
+            className={`account-link ${
+              selectedLink === "post" ? "selected-acc-link" : ""
+            }`}
+            onClick={() => handleLinkClick("post")}
+            href="myaccount-post"
+          >
+            Post
+          </a>
+        </div>
+        <div id="section">
+          <a
+            className={`account-link ${
+              selectedLink === "transaction" ? "selected-acc-link" : ""
+            }`}
+            onClick={() => handleLinkClick("transaction")}
+            href="myaccount-transaction"
+          >
+            My Transaction
+          </a>
+        </div>
+        <div id="section">
+          <a
+            className={`account-link ${
+              selectedLink === "details" ? "selected-acc-link" : ""
+            }`}
+            onClick={() => handleLinkClick("details")}
+            href="myaccount-details"
+          >
+            About
+          </a>
+        </div>
       </section>
     </div>
   );
