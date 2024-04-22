51a955d490b1534b8da32814db4d4da8c4cf1cb1
Razin <razinnazarudin@gmail.com>
2024-04-22 22:33:03 +0800

    Betulkan profile tapi tak boleh

--- src/Pages/general/MyAccountHeader.js
+++ src/Pages/general/MyAccountHeader.js
@@ -1,11 +1,11 @@
 import "./MyAccountHeader.css";
-import {ViewAccountProperty} from "../Property/ViewAccountProperty";
-import {ViewAccountPost} from "../Property/ViewAccountPost";
-import {ViewAccountAbout} from "../Property/ViewAccountAbout";
+import { ViewAccountProperty } from "../Property/ViewAccountProperty";
+import { ViewAccountPost } from "../Property/ViewAccountPost";
+import { ViewAccountAbout } from "../Property/ViewAccountAbout";
 import profilePic from "../../Res/image/user profile.png";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export const MyAccountHeader = ({name, bio, /*currentPage*/}) => {
+export const MyAccountHeader = ({ name, bio /*currentPage*/ }) => {
   const [selectedLink, setSelectedLink] = useState(null);
 
   const handleLinkClick = (link) => {
@@ -15,49 +15,54 @@ export const MyAccountHeader = ({name, bio, /*currentPage*/}) => {
   return (
     <div className="MyAccountHeader">
       <section className="flex" id="details-vah">
-        <div >
-           <img 
-            id="user-image"
-            src={require("../../Res/image/user profile.png")}
-          />
-        </div>
-        <div className="MyAccountHeader-desc">
-          <h1 style={{fontSize: "1.7em"}}>{name}</h1>
-          <p style={{fontSize: "1.0em"}}>{bio}</p>
+        <img
+          id="user-image"
+          className="profile-image"
+          src={require("../../Res/image/user profile.png")}
+        />
 
+        <div className="MyAccountHeader-desc">
+          <h1 style={{ fontSize: "1.7em" }}>{name}</h1>
+          <p style={{ fontSize: "1.0em" }}>{bio}</p>
         </div>
         <a href="manage-account">
-          <img
-          src={require("../../Res/image/editicon.jpg")}
-          /></a>
-
+          <img src={require("../../Res/image/editicon.jpg")} />
+        </a>
       </section>
       <hr />
       <section id="account-link-container">
-        <a 
-          className={`account-link ${selectedLink === 'property' ? 'selected-acc-link' : ''}`}
-          onClick={() => handleLinkClick('property')}
+        <a
+          className={`account-link ${
+            selectedLink === "property" ? "selected-acc-link" : ""
+          }`}
+          onClick={() => handleLinkClick("property")}
           href="myaccount-property"
         >
           Property
         </a>
-        <a 
-          className={`account-link ${selectedLink === 'post' ? 'selected-acc-link' : ''}`}
-          onClick={() => handleLinkClick('post')}
+        <a
+          className={`account-link ${
+            selectedLink === "post" ? "selected-acc-link" : ""
+          }`}
+          onClick={() => handleLinkClick("post")}
           href="myaccount-post"
         >
           Post
         </a>
-        <a 
-          className={`account-link ${selectedLink === 'transaction' ? 'selected-acc-link' : ''}`}
-          onClick={() => handleLinkClick('transaction')}
+        <a
+          className={`account-link ${
+            selectedLink === "transaction" ? "selected-acc-link" : ""
+          }`}
+          onClick={() => handleLinkClick("transaction")}
           href="myaccount-transaction"
         >
           My Transaction
         </a>
-        <a 
-          className={`account-link ${selectedLink === 'details' ? 'selected-acc-link' : ''}`}
-          onClick={() => handleLinkClick('details')}
+        <a
+          className={`account-link ${
+            selectedLink === "details" ? "selected-acc-link" : ""
+          }`}
+          onClick={() => handleLinkClick("details")}
           href="myaccount-details"
         >
           About
@@ -65,7 +70,6 @@ export const MyAccountHeader = ({name, bio, /*currentPage*/}) => {
       </section>
     </div>
   );
-
 
   //Azim-code
   // const [currentPage, setCurrentPage] = useState("Property");
