69ae349197238c47d0ba9a46c17763f6ded42152
azim <u2102134@siswa.um.edu.my>
2024-04-22 20:19:44 +0800

    mkae module css for ForumPage

--- src/Pages/Social/ForumPage.js
+++ src/Pages/Social/ForumPage.js
@@ -1,43 +1,43 @@
-import "./ForumPage.css";
+import styles from "./ForumPage.module.css";
 
-export const ForumPage = () => {
+const ForumPage = () => {
   return (
-    <div className="ForumPage">
-      <button className="share-post">
-        <div className="share-firstRow">
+    <div className={styles.ForumPage}>
+      <button className={styles['create-post']}>
+        <div className={styles['create-firstRow-container']}>
           <img
-            id="user-image"
+            id={styles['user-image']}
             src={require("../../Res/image/user profile.png")}
           />
-          <p id="user-text-button">What's on your mind Azim?</p>
+          <p id={styles['user-text-button']}>What's on your mind Azim?</p>
         </div>
-        <div className="share-secondRow">
-          <div className="box one">
+        <div className={styles['create-secondRow-container']}>
+          <div className={`${styles.box} ${styles.one}`}>
             <img
-              id="button-image"
+              id={styles['button-image']}
               src={require("../../Res/image/image icon.png")}
             />
-            <p id="button-text">Likes</p>
+            <p id={styles['button-text']}>Likes</p>
           </div>
-          <div className="box two">
-            <img id="button-image" src={require("../../Res/image/video.png")} />
-            <p id="button-text">Video</p>
+          <div className={`${styles.box} ${styles.two}`}>
+            <img id={styles['button-image']} src={require("../../Res/image/video.png")} />
+            <p id={styles['button-text']}>Video</p>
           </div>
         </div>
       </button>
-      <div className="topic-discuss">
-        <div className="user-profile">
+      <div className={styles['topic-discuss']}>
+        <div className={styles['user-profile']}>
           <img
-            id="user-image"
+            id={styles['user-image']}
             src={require("../../Res/image/user profile.png")}
           />
-          <section id="user-text">
-            <p id="user-name">Wan Harith</p>
-            <p id="user-time">10 min ago</p>
+          <section id={styles['user-text']}>
+            <p id={styles['user-name']}>Wan Harith</p>
+            <p id={styles['user-time']}>10 min ago</p>
           </section>
         </div>
-        <div className="user-text">
-          <p id="user-text-post">
+        <div className={styles['user-text']}>
+          <p id={styles['user-text-post']}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -47,38 +47,38 @@ export const ForumPage = () => {
             scelerisque vitae, consequat in, pretium a, enim.
           </p>
         </div>
-        <div className="user-button">
-          <button className="button">
-            <img id="img-button" src={require("../../Res/image/heart.png")} />
-            <p id="img-description">Likes</p>
+        <div className={styles['user-button']}>
+          <button className={styles.button}>
+            <img id={styles['img-button']} src={require("../../Res/image/heart.png")} />
+            <p id={styles['img-description']}>Likes</p>
           </button>
-          <button className="button">
+          <button className={styles.button}>
             <img
-              id="img-button"
+              id={styles['img-button']}
               src={require("../../Res/image/message-square.png")}
             />
-            <p id="img-description">Likes</p>
+            <p id={styles['img-description']}>Likes</p>
           </button>
-          <button className="button">
-            <img id="img-button" src={require("../../Res/image/share-2.png")} />
-            <p id="img-description">Likes</p>
+          <button className={styles.button}>
+            <img id={styles['img-button']} src={require("../../Res/image/share-2.png")} />
+            <p id={styles['img-description']}>Likes</p>
           </button>
         </div>
       </div>
 
-      <div className="topic-discuss">
-        <div className="user-profile">
+      <div className={styles['topic-discuss']}>
+        <div className={styles['user-profile']}>
           <img
-            id="user-image"
+            id={styles['user-image']}
             src={require("../../Res/image/user profile.png")}
           />
-          <section id="user-text">
-            <p id="user-name">Azim</p>
-            <p id="user-time">22 min ago</p>
+          <section id={styles['user-text']}>
+            <p id={styles['user-name']}>Wan Harith</p>
+            <p id={styles['user-time']}>10 min ago</p>
           </section>
         </div>
-        <div className="user-text">
-          <p id="user-text-post">
+        <div className={styles['user-text']}>
+          <p id={styles['user-text-post']}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -88,24 +88,26 @@ export const ForumPage = () => {
             scelerisque vitae, consequat in, pretium a, enim.
           </p>
         </div>
-        <div className="user-button">
-          <button className="button">
-            <img id="img-button" src={require("../../Res/image/heart.png")} />
-            <p id="img-description">Likes</p>
+        <div className={styles['user-button']}>
+          <button className={styles.button}>
+            <img id={styles['img-button']} src={require("../../Res/image/heart.png")} />
+            <p id={styles['img-description']}>Likes</p>
           </button>
-          <button className="button">
+          <button className={styles.button}>
             <img
-              id="img-button"
+              id={styles['img-button']}
               src={require("../../Res/image/message-square.png")}
             />
-            <p id="img-description">Likes</p>
+            <p id={styles['img-description']}>Likes</p>
           </button>
-          <button className="button">
-            <img id="img-button" src={require("../../Res/image/share-2.png")} />
-            <p id="img-description">Likes</p>
+          <button className={styles.button}>
+            <img id={styles['img-button']} src={require("../../Res/image/share-2.png")} />
+            <p id={styles['img-description']}>Likes</p>
           </button>
         </div>
       </div>
     </div>
   );
 };
+
+export {ForumPage}
