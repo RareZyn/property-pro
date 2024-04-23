e0b1ec7b6dbc3c5715916c31c5e61d858c6dd0b1
Wan2729 <wanharithazdy12@gmail.com>
2024-04-23 17:35:31 +0800

    Adjust

--- src/Cards/Property Cards/PropertyDisplayCard.jsx
+++ src/Cards/Property Cards/PropertyDisplayCard.jsx
@@ -2,6 +2,7 @@ import "./PropertyDisplayCard.css";
 import imgPh from "../../Res/image/image-dummy-house.png"
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const PropertyDisplayCard = ({thumbnailImage}) => {
     const [thumbnail, setThumbnail] = useState(imgPh);
@@ -17,15 +18,17 @@ export const PropertyDisplayCard = ({thumbnailImage}) => {
     }, [thumbnailImage]);
     
     return(
-        <div id="prop-card">
-        <div id="img-placeholder">
-           <img src={thumbnail} alt="Image" srcset="" id="img-ph"/>
-        </div>
-            <div id="details">
-                <span>Item name</span>
-                <span style={{fontWeight:"bold"}}>RM XX.XX</span>
-            </div>
-        </div>
+        <Link to="/property-details">
+            <div id="prop-card">
+                <div id="img-placeholder">
+                <img src={thumbnail} alt="Image" srcset="" id="img-ph"/>
+                </div>
+                    <div id="details">
+                        <span>Item name</span>
+                        <span style={{fontWeight:"bold"}}>RM XX.XX</span>
+                    </div>
+            </div> 
+        </Link>
     )
 }
 
