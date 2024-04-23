9cf8bede8ea8beb399033c10f782b96f584a5d12
azim <u2102134@siswa.um.edu.my>
2024-04-23 13:49:11 +0800

    fixing bug

--- src/Pages/general/LaunchPage.js
+++ src/Pages/general/LaunchPage.js
@@ -14,30 +14,57 @@ export const LaunchPage = () => {
     setHovered(!hovered);
   };
 
+  // return (
+  //   <div className={`launchPage ${loaded ? "loaded" : ""}`}>
+  //     <section className="home" id="home">
+  //       <div className="content">
+  //         <h1 id="website-name">PropertyPro+</h1>
+  //         <h1
+  //           id="launch-topic"
+  //           className={`${hovered ? "hovered" : ""}`}
+  //           onMouseEnter={handleHover}
+  //           onMouseLeave={handleHover}>
+  //           IT'S NEVER TOO LATE TO SPEND, NEVER TOO LATE TO SELL
+  //         </h1>
+
+  //         <div className="btn-container">
+  //           <Link id="button-register" to="/register">
+  //             <button className="btn register">Register</button>
+  //           </Link>
+  //           <Link id="button-login" to="/login">
+  //             <button className="btn login">Login</button>
+  //           </Link>
+  //         </div>
+  //       </div>
+  //     </section>
+  //   </div>
+  // );
+
   return (
     <div className={`launchPage ${loaded ? "loaded" : ""}`}>
-      <section className="home" id="home">
-        <div className="content">
-          <h1 id="website-name">PropertyPro+</h1>
-          <h1
-            id="launch-topic"
-            className={`${hovered ? "hovered" : ""}`}
-            onMouseEnter={handleHover}
-            onMouseLeave={handleHover}
-          >
-            IT'S NEVER TOO LATE TO SPEND, NEVER TOO LATE TO SELL
-          </h1>
+      <div className="content">
+        <h1 id="website-name">PropertyPro+</h1>
+        <h1
+          id="launch-topic"
+          className={`${hovered ? "hovered" : ""}`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHover}>
+          IT'S NEVER TOO LATE TO SPEND, NEVER TOO LATE TO SELL
+        </h1>
+        <span>Lorem ipsum</span>
 
-          <div className="btn-container">
-            <Link id="button-register" to="/register">
-              <button className="btn register">Register</button>
-            </Link>
-            <Link id="button-login" to="/login">
-              <button className="btn login">Login</button>
-            </Link>
-          </div>
+        <div className="btn-container">
+          <Link id="button-register" to="/register">
+            <button className="btn register">Register</button>
+          </Link>
+          <Link id="button-login" to="/login">
+            <button className="btn login">Login</button>
+          </Link>
         </div>
-      </section>
+      </div>
+      
+      <div id="launch-image-container">
+        <img src={require("../../Res/image/launch-image.png")} alt="" srcset="" /></div>
     </div>
   );
 };
