312608f3a1602e8e2dc772e534b386c4f8e589c8
Razin <razinnazarudin@gmail.com>
2024-04-24 02:35:58 +0800

    Fix my account header

--- src/Pages/general/MyAccountHeader.js
+++ src/Pages/general/MyAccountHeader.js
@@ -20,19 +20,19 @@ export const MyAccountHeader = () => {
   const [currentPage, setCurrentPage] = useState("Property");
   let page;
 
-  switch(currentPage){
+  switch (currentPage) {
     case "Property":
-      page = <MyAccountProperty />
+      page = <MyAccountProperty />;
       break;
     case "Post":
-      page = <MyAccountPost />
+      page = <MyAccountPost />;
       break;
     case "My Transaction":
-      page = <MyAccountTransaction />
+      page = <MyAccountTransaction />;
       break;
-      case "About":
-        page = <MyAccountDetails />
-        break;
+    case "About":
+      page = <MyAccountDetails />;
+      break;
   }
 
   return (
@@ -40,23 +40,56 @@ export const MyAccountHeader = () => {
       <div className="ViewAccountHeader">
         <section className="flex" id="details-vah">
           <div id="pp-container">
-            <img src={require("../../Res/image/user profile.png")} alt="" srcset="" />
+            <img
+              src={require("../../Res/image/user profile.png")}
+              alt=""
+              srcset=""
+            />
           </div>
           {/* <ProfilePicture imxgSrc={require("../../Res/image/user-image.png")} size={"10%"}/> */}
           <div className="acc-desc">
-            <h1 >{userDetails.username}</h1>
+            <h1>{userDetails.username}</h1>
             <span>{userDetails.bio}</span>
           </div>
           <a href="manage-account" id="edit-account">
-            <img src={require("../../Res/image/edit-icon.png")} alt="Edit icon"/>
+            <img
+              src={require("../../Res/image/edit-icon.png")}
+              alt="Edit icon"
+            />
           </a>
         </section>
         <hr />
         <section id="account-link-container">
-          <a className={`account-link ${currentPage === "Property" ? "active" : ""}`} onClick={() => setCurrentPage("Property")}>Property</a>
-          <a className={`account-link ${currentPage === "Post" ? "active" : ""}`} onClick={() => setCurrentPage("Post")}>Post</a>
-          <a className={`account-link ${currentPage === "My Transaction" ? "active" : ""}`} onClick={() => setCurrentPage("My Transaction")}>My Transaction</a>
-          <a className={`account-link ${currentPage === "About" ? "active" : ""}`} onClick={() => setCurrentPage("About")}>About</a>
+          <a
+            className={`account-link ${
+              currentPage === "Property" ? "active" : ""
+            }`}
+            onClick={() => setCurrentPage("Property")}
+          >
+            Property
+          </a>
+          <a
+            className={`account-link ${currentPage === "Post" ? "active" : ""}`}
+            onClick={() => setCurrentPage("Post")}
+          >
+            Post
+          </a>
+          <a
+            className={`account-link ${
+              currentPage === "My Transaction" ? "active" : ""
+            }`}
+            onClick={() => setCurrentPage("My Transaction")}
+          >
+            My Transaction
+          </a>
+          <a
+            className={`account-link ${
+              currentPage === "About" ? "active" : ""
+            }`}
+            onClick={() => setCurrentPage("About")}
+          >
+            About
+          </a>
         </section>
       </div>
       {page}
--- src/Pages/general/MyAccountPost.js
+++ src/Pages/general/MyAccountPost.js
@@ -1,134 +1,134 @@
 import "./MyAccountPost.css";
 import PostCard from "../../Cards/Posting Cards/PostCard";
 import AddPostCard from "../../Cards/Posting Cards/AddPostCard";
-import { MyAccountHeader } from "./MyAccountHeader";
-import { NavHeader } from "../Navigation/NavHeader";
+import { Link } from "react-router-dom";
 
 export const MyAccountPost = () => {
-//   return (
-//     <div className="acc-grid-container">
-//   <div className="MyAccountPost-page">
-//       <button className="share-post">
-//         <div className="share-firstRow">
-//           <img
-//             id="user-image"
-//             src={require("../../Res/image/user profile.png")}
-//           />
-//           <p id="user-text-button">Post something....</p>
-//         </div>
-//         <div className="share-secondRow">
-//           <div className="box one">
-//             <img
-//               id="button-image"
-//               src={require("../../Res/image/image icon.png")}
-//             />
-//             <p id="button-text">Likes</p>
-//           </div>
-//           <div className="box two">
-//             <img id="button-image" src={require("../../Res/image/video.png")} />
-//             <p id="button-text">Video</p>
-//           </div>
-//         </div>
-//       </button>
-   
-// <div className="mypost">
-//         <div className="user-profile">
-//           <img
-//             id="user-image"
-//             src={require("../../Res/image/user profile.png")}
-//           />
-//           <section id="user-text">
-//             <p id="user-name">Username</p>
-//             <p id="user-time">10 min ago</p>
-//           </section>
-//         </div>
-//         <div className="user-text">
-//           <p id="user-text-post">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//             ligula massa, varius a, semper congue, euismod non, mi. Proin
-//             porttitor, orci nec nonummy molestie, enim est eleifend mi, non
-//             fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa,
-//             scelerisque vitae, consequat in, pretium a, enim.
-//           </p>
-//         </div>
-//         <div className="user-button">
-//           <button className="button">
-//             <img id="img-button" src={require("../../Res/image/heart.png")} />
-//             <p id="img-description">Likes</p>
-//           </button>
-//           <button className="button">
-//             <img
-//               id="img-button"
-//               src={require("../../Res/image/message-square.png")}
-//             />
-//             <p id="img-description">Comment</p>
-//           </button>
-//           <button className="button">
-//             <img id="img-button" src={require("../../Res/image/share-2.png")} />
-//             <p id="img-description">Share</p>
-//           </button>
-//         </div>
-//       </div>
+  //   return (
+  //     <div className="acc-grid-container">
+  //   <div className="MyAccountPost-page">
+  //       <button className="share-post">
+  //         <div className="share-firstRow">
+  //           <img
+  //             id="user-image"
+  //             src={require("../../Res/image/user profile.png")}
+  //           />
+  //           <p id="user-text-button">Post something....</p>
+  //         </div>
+  //         <div className="share-secondRow">
+  //           <div className="box one">
+  //             <img
+  //               id="button-image"
+  //               src={require("../../Res/image/image icon.png")}
+  //             />
+  //             <p id="button-text">Likes</p>
+  //           </div>
+  //           <div className="box two">
+  //             <img id="button-image" src={require("../../Res/image/video.png")} />
+  //             <p id="button-text">Video</p>
+  //           </div>
+  //         </div>
+  //       </button>
 
+  // <div className="mypost">
+  //         <div className="user-profile">
+  //           <img
+  //             id="user-image"
+  //             src={require("../../Res/image/user profile.png")}
+  //           />
+  //           <section id="user-text">
+  //             <p id="user-name">Username</p>
+  //             <p id="user-time">10 min ago</p>
+  //           </section>
+  //         </div>
+  //         <div className="user-text">
+  //           <p id="user-text-post">
+  //             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+  //             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+  //             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
+  //             ligula massa, varius a, semper congue, euismod non, mi. Proin
+  //             porttitor, orci nec nonummy molestie, enim est eleifend mi, non
+  //             fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa,
+  //             scelerisque vitae, consequat in, pretium a, enim.
+  //           </p>
+  //         </div>
+  //         <div className="user-button">
+  //           <button className="button">
+  //             <img id="img-button" src={require("../../Res/image/heart.png")} />
+  //             <p id="img-description">Likes</p>
+  //           </button>
+  //           <button className="button">
+  //             <img
+  //               id="img-button"
+  //               src={require("../../Res/image/message-square.png")}
+  //             />
+  //             <p id="img-description">Comment</p>
+  //           </button>
+  //           <button className="button">
+  //             <img id="img-button" src={require("../../Res/image/share-2.png")} />
+  //             <p id="img-description">Share</p>
+  //           </button>
+  //         </div>
+  //       </div>
 
-//         <div className="mypost">
-//           <div className="user-profile">
-//             <img
-//               id="user-image"
-//               src={require("../../Res/image/user profile.png")}
-//             />
-//             <section id="user-text">
-//               <p id="user-name">Username</p>
-//               <p id="user-time">10 min ago</p>
-//             </section>
-//           </div>
-//           <div className="user-text">
-//             <p id="user-text-post">
-//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-//               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-//               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-//               ligula massa, varius a, semper congue, euismod non, mi. Proin
-//               porttitor, orci nec nonummy molestie, enim est eleifend mi, non
-//               fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa,
-//               scelerisque vitae, consequat in, pretium a, enim.
-//             </p>
-//           </div>
-//           <div className="user-button">
-//             <button className="button">
-//               <img id="img-button" src={require("../../Res/image/heart.png")} />
-//               <p id="img-description">Likes</p>
-//             </button>
-//             <button className="button">
-//               <img
-//                 id="img-button"
-//                 src={require("../../Res/image/message-square.png")}
-//               />
-//               <p id="img-description">Comment</p>
-//             </button>
-//             <button className="button">
-//               <img
-//                 id="img-button"
-//                 src={require("../../Res/image/share-2.png")}
-//               />
-//               <p id="img-description">Share</p>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
+  //         <div className="mypost">
+  //           <div className="user-profile">
+  //             <img
+  //               id="user-image"
+  //               src={require("../../Res/image/user profile.png")}
+  //             />
+  //             <section id="user-text">
+  //               <p id="user-name">Username</p>
+  //               <p id="user-time">10 min ago</p>
+  //             </section>
+  //           </div>
+  //           <div className="user-text">
+  //             <p id="user-text-post">
+  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+  //               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+  //               nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
+  //               ligula massa, varius a, semper congue, euismod non, mi. Proin
+  //               porttitor, orci nec nonummy molestie, enim est eleifend mi, non
+  //               fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa,
+  //               scelerisque vitae, consequat in, pretium a, enim.
+  //             </p>
+  //           </div>
+  //           <div className="user-button">
+  //             <button className="button">
+  //               <img id="img-button" src={require("../../Res/image/heart.png")} />
+  //               <p id="img-description">Likes</p>
+  //             </button>
+  //             <button className="button">
+  //               <img
+  //                 id="img-button"
+  //                 src={require("../../Res/image/message-square.png")}
+  //               />
+  //               <p id="img-description">Comment</p>
+  //             </button>
+  //             <button className="button">
+  //               <img
+  //                 id="img-button"
+  //                 src={require("../../Res/image/share-2.png")}
+  //               />
+  //               <p id="img-description">Share</p>
+  //             </button>
+  //           </div>
+  //         </div>
+  //       </div>
   //   </div>
   // );
 
-  let items= [];
-  for(let i=0 ; i <3 ; i++){
-    items.push(<PostCard/>)
+  let items = [];
+  for (let i = 0; i < 3; i++) {
+    items.push(<PostCard />);
   }
 
-  return(
+  return (
     <div className="acc-grid-container">
-      <AddPostCard />
+      <Link to="/forum-page" id="acc-grid">
+        <AddPostCard />
+      </Link>
       {items}
     </div>
-  )
+  );
 };
