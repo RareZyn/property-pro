352651d7d06f160ecd827e6c09367e5b149d5db6
Razin <razinnazarudin@gmail.com>
2024-04-24 00:05:37 +0800

    Broker Registeration

--- src/Pages/Broker/LoginBroker.js
+++ src/Pages/Broker/LoginBroker.js
@@ -4,31 +4,30 @@ export const LoginBroker = () => {
   return (
     <div className="LoginBroker">
       <div className="LoginBrokerCard">
-        <h3>BROKER registration</h3>
-        <h5>In broker we trust</h5>
-        <RegisterBrokerInputItem inputTitle="Email" inputHint=""></RegisterBrokerInputItem> 
-        <RegisterBrokerInputItem inputTitle="Password" inputHint=""></RegisterBrokerInputItem>
-        <button>
-          <a href="./verify-property-homepage">
-            Login
-          </a>
-        </button>
-        <div className="RegisterBrokerToLogin">
-          <p>Don't have an account?</p>
-          <a href="./register-broker">Register</a>
+        <div className="login-header">
+          <h1>Broker Registration</h1>
+          <h3>In broker we trust</h3>
+        </div>
+        <div className="broker-login">
+          <section className="broker-input">
+            <span>Email</span>
+            <input type="email" id="broker-email" />
+          </section>
+          <section className="broker-input">
+            <span>Password</span>
+            <input type="password" id="broker-password" />
+          </section>
+          <div className="button-section">
+            <button id="broker-login-button">
+              <a href="./verify-property-homepage">Login</a>
+            </button>
+            <div className="RegisterBrokerToLogin">
+              <p>Don't have an account?</p>
+              <a href="./register-broker">Register</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
-function RegisterBrokerInputItem({inputTitle,inputHint}){
-  return(
-    <div className="InputDetails">
-      <h7>{inputTitle}</h7>
-      <div className="Input">
-        {inputHint===""? "Enter your "+inputTitle : {inputHint}}
-      </div>
-    </div>
-  )
-}
