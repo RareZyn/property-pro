01abba5de99b55085f44d932ad0919ff8823214e
Razin <razinnazarudin@gmail.com>
2024-04-24 01:08:40 +0800

    Betulkan forum

--- src/Pages/Social/ForumPage.js
+++ src/Pages/Social/ForumPage.js
@@ -4,29 +4,30 @@ import likeIcon from "../../Res/image/heart.png";
 import likedIcon from "../../Res/image/red-heart.png";
 import { ForumHeader } from "./ForumHeader";
 import PostCard from "../../Cards/Posting Cards/PostCard";
+import { ForumCreatePost } from "./ForumCreatePost";
 
 const ForumPage = () => {
   let postCardItems = [];
-  for (let i=0;i<5;i++){
+  for (let i = 0; i < 5; i++) {
     postCardItems.push(<PostCardCommunity></PostCardCommunity>);
   }
 
   return (
     <>
-    <ForumHeader/>
-    {postCardItems}
+      <ForumHeader />
+      <ForumCreatePost />
+      {postCardItems}
     </>
   );
   // l
 };
 
-export {ForumPage}
+export { ForumPage };
 
-const PostCardCommunity = () =>{
-  return(
+const PostCardCommunity = () => {
+  return (
     <div className={styles["PostCardContainer"]}>
       <PostCard></PostCard>
     </div>
-  )
-}
-
+  );
+};
