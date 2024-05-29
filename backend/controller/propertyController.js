c9b37b6b41ca93423ba7030b6cb0b8f33ad2c137
Razin <razinnazarudin@gmail.com>
2024-05-29 21:56:57 +0800

    update prisma

---
+++ backend/controller/propertyController.js
@@ -0,0 +1,2 @@
+const asyncHandler = require("express-async-handler");
+const { prisma } = require("../config/prismaConfig.js");
---
+++ backend/controller/userController.js
@@ -0,0 +1,17 @@
+const asyncHandler = require("express-async-handler");
+const { prisma } = require("../config/prismaConfig.js");
+
+const findUser = asyncHandler(async (req, res) => {
+  try {
+    const users = await prisma.users.findMany({
+      where: {},
+    });
+    console.log("Users:", users); 
+    res.status(200).json(users); 
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ error: err.message }); 
+  }
+});
+
+module.exports = { findUser };
