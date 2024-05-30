4fa5228897e4c0e7cdfdd4dc1459058185a95e07
EZYANZAINUDDIN <u2000643@siswa.um.edu.my>
2024-05-30 20:23:29 +0800

    popup

--- src/Cards/General Cards/PopupShare.jsx
+++ src/Cards/General Cards/PopupShare.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import "./PopupShare.css";
 
  
 export default function PopupShare() {
     return (
         <div>
             <Popup trigger=
-                {<button> Click to open modal </button>} 
+                {<button id='ShareBtn'> Share </button>} 
                 modal nested>
                 {
                     close => (
                         <div className='modal'>
                             <div className='content'>
-                                Welcome to GFG!!!
+                                Welcome to GFG!!!zshbhdbshfbhdbfhbdhj
                             </div>
+                        
                             <div>
                                 <button onClick=
                                     {() => close()}>
