23cd6a7cebf8a5e5c5469e752a9c16d821f7f1dc
Aufanatasha <u2102739@siswa.um.edu.my>
2024-05-31 03:07:55 +0800

    savedProperty,fix grid

--- src/Pages/Property/SavedProperty.js
+++ src/Pages/Property/SavedProperty.js
@@ -1,21 +1,25 @@
-import "./SavedProperty.css";
+import style from "./SavedProperty.css";
 import { PropertyDisplayCard } from "../../Cards/Property Cards/PropertyDisplayCard";
-import backIcon from "../../Res/image/back-icon.png";
+import "./GeneralProperty.css";
+
 
 export const SavedProperty = () => {
   const propertyCards = [];
   let color = 0;
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 6; i++) {
+    color = i - 6 * parseInt(i / 6);
+    console.log(color);
     propertyCards.push(<PropertyDisplayCard link={"/property-details"} />);
   }
 
   return (
-    <div className="SavedProperty">
-      <div className="properties-grid">
-        <h1 className="property-headline">Saved Property</h1>
-        {propertyCards}
+    <div>
+        <div className="property-headline">Saved Property</div>
+        <div className="properties-grid">
+          {propertyCards}
+        </div>
       </div>
-    </div>
   );
 };
+
