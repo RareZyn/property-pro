73566d35610c0875ab543b58060d54380019eb9b
EZYANZAINUDDIN <u2000643@siswa.um.edu.my>
2024-05-31 23:15:53 +0800

    ItemComponentCard

--- src/Cards/Property Cards/VehicleComponentCard.jsx
+++ src/Cards/Property Cards/VehicleComponentCard.jsx
@@ -1,6 +1,7 @@
 import "./ItemComponentCard.css";
 import { Link } from "react-router-dom";
-import { FaShower, FaBed } from "react-icons/fa";
+import { FaShower, FaBed, FaCarSide, FaTags } from "react-icons/fa";
+import { FaCalendarDays, FaGauge, FaHouse, FaLocationDot, FaTag } from "react-icons/fa6";
 
 function VehicleComponentCard({imgLink, link}){
     return(
@@ -10,12 +11,25 @@ function VehicleComponentCard({imgLink, link}){
                     <img src={imgLink} alt="image property" srcset="" />
                 </section>
                 <div id="details-container">
-                    <div id="myproperty-name">Vehicle</div>
-                    <div id="myproperty-location">Gua Musang, Kelantan</div>
-                    <div id="myproperty-price">Price : RM 162,300,000</div>
-                    <div id="myproperty-bedroom-bathroom">
-                        <div id="myproperty-noOf-bedroom"><FaBed/> 2</div>
-                        <div id="myproperty-noOf-bathroom"><FaShower/> 2</div>
+                    <div id="myproperty-name"><h4>Vehicle</h4></div>
+                    <div id="myproperty-location"><FaLocationDot/>  Gua Musang, Kelantan</div>
+                    <div id="myproperty-price"><FaTags/>  RM 162,300,000</div>
+                    <div id="mypropertyicon-content">
+                        <div id="property-smallicon">
+                            <FaTags/> New
+                        </div>
+                        <div id="property-smallicon">
+                            <FaGauge/> 200cc
+                        </div>
+                    </div>
+                    <div id="mypropertyicon-content">
+                        
+                        <div id="property-smallicon">
+                            <FaCalendarDays /> 2023
+                        </div>
+                        <div id="property-smallicon">
+                            <FaCarSide/> 4 seat
+                        </div>
                     </div>
                     {/**<span id="row-3">3210 ekar (Tanah) • RM X.XX PSF</span>**/}
                 </div>
