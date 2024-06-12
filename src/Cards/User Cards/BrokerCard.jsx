4d1730f715a408d6787c5259de1e1426d9563983
Wan2729 <wanharithazdy12@gmail.com>
2024-06-12 08:40:48 +0800

    Can now chat broker

--- src/Cards/User Cards/BrokerCard.jsx
+++ src/Cards/User Cards/BrokerCard.jsx
@@ -1,9 +1,9 @@
 import ProfilePicture from '../Image Placeholder/ProfilePicture';
 import styles from './BrokerCards.module.css';
 
-function BrokerCard({name, location,image}){
+function BrokerCard({name, location,image, onCLick}){
     return (
-      <div className={`${styles["broker-card-container"]}`}>
+      <div className={`${styles["broker-card-container"]}`} onClick={onCLick}>
         <ProfilePicture size={"5rem"} imgLink={image} />
         <div className={`${styles["broker-details"]}`}>
           <h1>{name ? name : "Name"}</h1>
--- src/Pages/Broker/BrokerList.js
+++ src/Pages/Broker/BrokerList.js
@@ -1,13 +1,16 @@
 import styles from './BrokerList.module.css';
 import BrokerCard from '../../Cards/User Cards/BrokerCard'
 import SearchBar from '../../Cards/General Cards/SearchBar';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useContext } from 'react';
 import { getAllBrokers } from '../../utils/api';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
+import ChatContext from '../../context/ChatContext';
+import { UserContext } from '../../context/UserContext';
+import { getUser } from '../../utils/userAPI';
 
 function BrokerList(){
     const [brokers, setBrokers] = useState([]);
-
+    
     useEffect(() => {
       const fetchBrokers = async () => {
         try {
@@ -20,8 +23,47 @@ function BrokerList(){
   
       fetchBrokers();
     }, []);
-
     console.log(brokers)
+    
+    // To handle fetch user
+    const { userToken } = useContext(UserContext);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const userData = await getUser(userToken);
+          setUser(userData);
+        } catch (error) {
+          console.error("Failed to fetch user data:", error);
+        }
+      };
+  
+      if (userToken) {
+        fetchUser();
+      } else {
+        console.log("No user token");
+      }
+    }, [userToken]);
+  
+    const userID = user?._id;
+    // End fetch user
+
+    // To handle chat vv
+    const { createRoom } = useContext(ChatContext);
+    const navigate = useNavigate()
+
+    const createChatRoom = async (currentBrokerID) => {
+      console.log("Broker ID:",currentBrokerID);
+      try{
+        await createRoom(userID,currentBrokerID);
+        navigate("/chat");
+      } catch(error){
+        console.error("Error Create Chat Room {PropertyVehicleDetails}: ",error);
+      }
+    };
+    // End handle chat ^^
+
     return(
         <div className={`header-footer-wrap ${styles['broker-list-container']}`}>
             <div className={`${styles['broker-list-header']}`}>
@@ -30,9 +72,11 @@ function BrokerList(){
             </div>
             <div className={`${styles['broker-list']}`}>
                 {brokers?.map((broker) => (
-                    <Link>
-                        <BrokerCard name={broker.fullName} location={broker.user.location} image={broker.user.profilePicture} />
-                    </Link>
+                  <Link to={"/chat"}>
+                    <div onClick={() => createChatRoom(broker.user.id)}>
+                        <BrokerCard name={broker.fullName} location={broker.user.location} image={broker.user.profilePicture}/>
+                    </div>
+                  </Link>
                 ))}
             </div>
         </div>
